b50d7182782f9a18235d8b486073ed0000231cba
ong <ong2323@gmail.com>
2021-01-16 21:03:35 -0800

    Added email capabilities using smtp

--- e-card_writer.py
+++ e-card_writer.py
@@ -1,10 +1,16 @@
-import gspread
+import gspread, sys
 from oauth2client.service_account import ServiceAccountCredentials
 from PIL import Image, ImageFont, ImageDraw
-import sys
+import smtplib, ssl , getpass, email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#STEPS: Get Data -> Make Card -> Send Card -> Update Data
 
 #ACCESS GOOGLE DRIVE
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/gmail.send"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("MTSA eCard registration - Form Responses").sheet1
@@ -22,6 +28,9 @@ if name_index ==[]:
 else:
     row = name_index[0]
 
+
+
+
 #make new card
 name = sheet.cell(row,2).value +" " + sheet.cell(row,3).value
 card = Image.open("new_card.jpg")
@@ -31,11 +40,62 @@ W, H = name_font.getsize(name)
 edit_card.text(((2363-W)/2,1330), name, (255,255,255), font=name_font)
 card.save("MTSA e-Card (Front).pdf")
 
-#update sent
+# Email section
+port = 465 #for SSL
+context = ssl.create_default_context() #SSL context
+
+password = getpass.getpass()
+receivermail = sheet.cell(row,4).value 
+sendermail = "ongzh23@gmail.com"
+
+if receivermail == "":
+    print("Email not submitted")
+    sys.exit()
+
+#Read template email and create message
+file = open("mailtemplate.txt", "r")
+template = file.read()
+file.close()
+body = "Dear"+" "+ name + ",\n" + template
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sendermail
+message["To"] = receivermail
+message["Subject"] = "MTSA eCard"
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+#attach Cards to email
+files = ["MTSA e-Card (Front).pdf", "MTSA e-Card (Back).pdf"]
+for file in files:
+    attachment = open(file, 'rb')
+    file_name = file
+    part = MIMEBase('application','octet-stream')
+    part.set_payload(attachment.read())
+    part.add_header('Content-Disposition',
+                    'attachment',
+                    filename=file_name)
+    encoders.encode_base64(part)
+    message.attach(part)
+
+#convert message to string
+message = message.as_string()
+
+
+#send the mail
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(sendermail, password)
+    server.sendmail(sendermail, receivermail, message)
+    server.quit()
+
+#update data
 sheet.update_cell(row,8, "yes")
 
-#how many more times to run script
+#how many unsent cards left. 
 print(len(name_index)-1)
+    
 
 
 
